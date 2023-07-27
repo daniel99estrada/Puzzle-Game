7f7c3cafacbd73fb5a43364dc9dcb35089042518
daniel99estrada <pildoestrada@gmail.com>
2023-07-27 15:48:46 -0400

    Added Swiping

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.InputSystem;
+using GG.Infrastructure.Utils.Swipe;
 
 public class InputHandler : MonoBehaviour
-{
+{   
+    public SwipeListener swipeListener;
+
     public event Action<Vector2> OnNewInput;
 
     public bool isMoving;
@@ -26,23 +29,55 @@ public class InputHandler : MonoBehaviour
     }
 
     private void OnEnable()
-    {
-        playerInput = new PlayerInput();
-        playerInput.Input.Move.performed += Move;
-        playerInput.Enable();
+    {   
+        swipeListener.OnSwipe.AddListener(OnSwipe);
+
+        // playerInput = new PlayerInput();
+        // playerInput.Input.Move.performed += Move;
+        // playerInput.Enable();
+    }
+
+    private void OnSwipe(string swipe)
+    {   
+        if (isMoving) return;
+
+        Vector2 movementInput = Vector2.zero;
+        
+        switch (swipe)
+        {
+            case "UpRight":
+                movementInput = new Vector2(1f, 0f);
+                break;
+            case "UpLeft":
+                movementInput = new Vector2(0f, 1f);
+                break;
+            case "DownRight":
+                movementInput = new Vector2(0f, -1f);
+                break;
+            case "DownLeft":
+                movementInput = new Vector2(-1f, 0f);
+                break;
+            default:
+                break;
+        }
+
+        OnNewInput?.Invoke(movementInput);   
+
     }
 
     private void OnDisable()
-    {
-        if (playerInput != null)
-            playerInput.Disable();
+    {   
+        swipeListener.OnSwipe.RemoveListener(OnSwipe);
+
+        // if (playerInput != null)
+        //     playerInput.Disable();
     }
 
-    private void Move(InputAction.CallbackContext context)
-    {
-        Vector2 movementInput = context.ReadValue<Vector2>();
-        if (isMoving) return;
+    // private void Move(InputAction.CallbackContext context)
+    // {
+    //     Vector2 movementInput = context.ReadValue<Vector2>();
+    //     if (isMoving) return;
 
-        OnNewInput?.Invoke(movementInput);        
-    }
+    //     OnNewInput?.Invoke(movementInput);        
+    // }
 }
